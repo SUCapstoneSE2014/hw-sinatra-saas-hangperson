a5d0f883aaabdf84a4103e3f27de1658c6ba0856
Rick Denman <denman@southwestern.edu>
2016-01-31 03:10:11 +0000

    final solution

--- app.rb
+++ app.rb
@@ -67,13 +67,19 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if params[:word] == @game.guesses
+      erb :win # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.wrong_guesses == 7
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
 end
